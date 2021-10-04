2a88215e3cea9a67d76315b19081a0fa9de74166
Mohit Sharma <msharma3@me.iitr.ac.in>
2021-10-04 14:25:32 +0530

    countSubsetSum

---
+++ countSubsetsum.java
@@ -0,0 +1,33 @@
+
+public class countSubsetsum {
+
+    public static int count(int arr[], int sum) {
+        int n = arr.length;
+        int t[][] = new int[n + 1][sum + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (i == 0)
+                    t[i][j] = 0;
+                if (j == 0)
+                    t[i][j] = 1;
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i - 1] <= j)
+                    t[i][j] = t[i - 1][j - arr[i - 1]] + t[i - 1][j];
+                else
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+
+        return t[n][sum];
+
+    }
+
+    public static void main(String args[]) {
+        System.out.println(countSubsetsum.count(new int[] { 2, 3, 5, 6, 8, 10 }, 10));
+    }
+}
